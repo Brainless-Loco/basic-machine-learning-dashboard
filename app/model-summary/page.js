b2051c018788e684c0933ae06cc4f2d849d6c857
Brainless-Loco <dev.tcdas@gmail.com>
2023-08-23 23:24:28 +0600

    Alternative for ImageBox implemented but lept with commented

--- app/model-summary/page.js
+++ app/model-summary/page.js
@@ -25,6 +25,22 @@ export default function page() {
             
             <ImageBox title={"Comparison With Related Works"} source={"/assets/Co.png"} height={520} width={500} />
             
+
+            {/* 
+              <Box sx={{display:'flex',justifyContent:'center',flexDirection:'row',height:'100%'}}>
+                <Box sx={{display:'flex',justifyContent:'center',width:'50%',flexWrap:'wrap',gap:'0px'}}>
+                  <ImageBox title={"Wordcloud"} source={"/assets/wordcloud.png"} height={500} width={520} />
+
+                  <ImageBox title={"Roberta Roc Curve"} source={"/assets/robertaroc.png"} height={500} width={500} />
+                  <ImageBox title={"Compartive Matrix Analysis"} source={"/assets/compartiveMatrixAnalysis.png"} height={520} width={600} />
+                </Box>
+                <Box sx={{display:'flex',justifyContent:'center',width:'50%',flexWrap:'wrap',gap:'0px'}}>
+
+                  <ImageBox title={"Roberta Update Confusion Matrix"} source={"/assets/robertaupdatecon.png"} height={400} width={600} />
+                  <ImageBox title={"Machinelearning Model Roc Curve analysis"} source={"/assets/MachinelearningModelRoc Curve analysis.png"} height={450} width={600} />
+                  <ImageBox title={"Comparison With Related Works"} source={"/assets/Co.png"} height={520} width={500} />
+                </Box>
+            */}
         </Box>
 
       </body>
